1e8d14d7e76d2f431f009186d3cf896d5466774d
Winnie-girl <heatherweb3@proton.me>
2025-10-18 16:42:47 -0400

    downloaded zombie and wall model

---
+++ assets/scene/entity-names.ts
@@ -0,0 +1,13 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  cornwall_glb = "cornwall.glb",
+  cornwall_glb_2 = "cornwall.glb_2",
+  cornwall_glb_2_3 = "cornwall.glb_2_3",
+  cornwall_glb_2_4 = "cornwall.glb_2_4",
+  girlzombie_glb = "girlzombie.glb",
+} 
